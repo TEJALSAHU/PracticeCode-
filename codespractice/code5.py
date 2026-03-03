3e502edb3c4d82497c24da8414c11c174e87794c
Tejal Sahu <stejal2607@gmail.com>
2026-03-03 19:57:56 +0530

    updated code

---
+++ codespractice/code5.py
@@ -0,0 +1,6 @@
+#WAP to reverse the first and last name of the user and print them in the format "Hello  <last name> <first name>"
+
+#reverse name
+fname = input("Input your first name :")
+lname = input("Input your last name :")
+print("Hello  " + lname + " " + fname)
