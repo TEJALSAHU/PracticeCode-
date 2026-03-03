d6fec9f7b1eaf45f021bf9e7e9653a447a5454eb
Tejal Sahu <stejal2607@gmail.com>
2026-03-03 20:00:49 +0530

    ex6

---
+++ codespractice/code6.py
@@ -0,0 +1,7 @@
+#WAP to accept a comma separated sequence of words as input and print the unique words in sorted form (alphanumerically)
+#list tuple generator
+values = input("Input some comma-separated numbers: ")
+list = values.split(",")
+tuple = tuple(list)
+print('List: ', list)
+print('Tuple :', tuple)
