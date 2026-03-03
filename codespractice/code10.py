033eedc50064719890acec4f6e0aa411d1aa96ba
Tejal Sahu <stejal2607@gmail.com>
2026-03-03 20:19:35 +0530

    updated code

--- codespractice/code10.py
+++ codespractice/code10.py
@@ -8,3 +8,6 @@ a = int(input("Input an integer: "))
 n1 = int("%s" % a)          # Convert 'a' to an integer
 n2 = int("%s%s" % (a, a))   # Concatenate 'a' with itself and convert to an integer
 n3 = int("%s%s%s" % (a, a, a))  # Concatenate 'a' with itself twice and convert to an integer
+
+# Calculate the sum of 'n1', 'n2', and 'n3' and print the result
+print(n1 + n2 + n3)
