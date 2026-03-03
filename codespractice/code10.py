6c0c86fed1a41095d663a7cd67ada7f69dc9666f
Tejal Sahu <stejal2607@gmail.com>
2026-03-03 20:09:23 +0530

    updated code

---
+++ codespractice/code10.py
@@ -0,0 +1,10 @@
+#WAP to calculate the value of n + nn + nnn, where n is an integer input by the user.
+
+
+#no. expansion calculator
+a = int(input("Input an integer: "))
+
+# Create new integers 'n1', 'n2', and 'n3' by concatenating 'a' with itself one, two, and three times, respectively
+n1 = int("%s" % a)          # Convert 'a' to an integer
+n2 = int("%s%s" % (a, a))   # Concatenate 'a' with itself and convert to an integer
+n3 = int("%s%s%s" % (a, a, a))  # Concatenate 'a' with itself twice and convert to an integer
