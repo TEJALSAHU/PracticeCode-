90bdb58035a60a49310f19c6fb814ac9f8e3039e
Tejal Sahu <stejal2607@gmail.com>
2026-03-03 20:04:50 +0530

    updated code

---
+++ codespractice/code8.py
@@ -0,0 +1,6 @@
+#WAP to display the first and last colors from the following list: color = ["red","green","white" ,"black"]
+
+
+#first and last color
+color = ["red","green","white","black"]
+print( "%s %s" % (color[0],color[-1]))
