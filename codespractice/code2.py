db2dfcd83f3c4abfe5c1796e75fde79d4a701c8d
Tejal Sahu <stejal2607@gmail.com>
2026-03-03 19:43:59 +0530

    ex2

---
+++ codespractice/code2.py
@@ -0,0 +1,14 @@
+#WAP to print python version
+import sys
+print("Python version")
+print(sys.version)
+
+
+#WAP to print the version info
+import sys
+print("version info")
+print(sys.version_info)
+
+#WAP to print the current python version
+import platform
+print(platform.python_version())
