0167d1e9e354a9de2185a0d7fe5f8527aacd5f4d
Tejal Sahu <stejal2607@gmail.com>
2026-03-03 19:29:22 +0530

    Updated code1

---
+++ codespractice/code1.py
@@ -0,0 +1,3 @@
+
+#WAP to print the following string in a specific format (see the output).
+print("Twinkle, twinkle, little star,\nHow I wonder what you are! ,\n\t\t Up above the world so high,\n\t \tLike a diamond in the sky, \n\t Twinkle, twinkle, little star, \n\t How I wonder what you are!")
